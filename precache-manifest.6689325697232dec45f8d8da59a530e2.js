b3f3694711a1e59ffa34ef81347a14ecf9366d99
Joey Gaitan <joeygaitan707@gmail.com>
2019-03-19 09:57:02 -0700

    Updates

---
+++ precache-manifest.6689325697232dec45f8d8da59a530e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa6390cf4b4ca8234241",
+    "url": "/react-bookstore/static/css/main.3190bb80.chunk.css"
+  },
+  {
+    "revision": "fa6390cf4b4ca8234241",
+    "url": "/react-bookstore/static/js/main.da8e6b0d.chunk.js"
+  },
+  {
+    "revision": "b8e11062849f44b0a064",
+    "url": "/react-bookstore/static/js/runtime~main.aa841d1b.js"
+  },
+  {
+    "revision": "2f28d87b029107c8d157",
+    "url": "/react-bookstore/static/js/2.0f8786ab.chunk.js"
+  },
+  {
+    "revision": "d0def6c77b0a67e3092acf0edb8092de",
+    "url": "/react-bookstore/index.html"
+  }
+];
